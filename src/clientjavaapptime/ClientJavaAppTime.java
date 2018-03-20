b9b3514a2fdffeae17b8d44088e445f48b9b91ff
gabrielcaballerosta <gabri.vecindario.2011@gmail.com>
2018-03-20 23:56:18 +0100

    Primer Commit ClientJavaAppTime

---
+++ src/clientjavaapptime/ClientJavaAppTime.java
@@ -0,0 +1,10 @@
+package clientjavaapptime;
+
+public class ClientJavaAppTime {
+
+    public static void main(String[] args) {
+        NJC njc = new NJC();
+        System.out.println(njc.getJson());
+    }
+    
+}
---
+++ src/clientjavaapptime/NJC.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package clientjavaapptime;
+
+import javax.ws.rs.ClientErrorException;
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.WebTarget;
+
+/**
+ * Jersey REST client generated for REST resource:GenericResource
+ * [horaservidor]<br>
+ * USAGE:
+ * <pre>
+ *        NJC client = new NJC();
+ *        Object response = client.XXX(...);
+ *        // do whatever with response
+ *        client.close();
+ * </pre>
+ *
+ * @author entrar
+ */
+public class NJC {
+    private WebTarget webTarget;
+    private Client client;
+    private static final String BASE_URI = "http://localhost:8080/WSDate/webresources";
+
+    public NJC() {
+        client = javax.ws.rs.client.ClientBuilder.newClient();
+        webTarget = client.target(BASE_URI).path("horaservidor");
+    }
+
+    public void putJson(Object requestEntity) throws ClientErrorException {
+        webTarget.request(javax.ws.rs.core.MediaType.APPLICATION_JSON).put(javax.ws.rs.client.Entity.entity(requestEntity, javax.ws.rs.core.MediaType.APPLICATION_JSON));
+    }
+
+    public String getJson() throws ClientErrorException {
+        WebTarget resource = webTarget;
+        return resource.request(javax.ws.rs.core.MediaType.APPLICATION_JSON).get(String.class);
+    }
+
+    public void close() {
+        client.close();
+    }
+    
+}
